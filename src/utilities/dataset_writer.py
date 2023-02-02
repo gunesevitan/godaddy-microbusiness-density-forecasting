9056312ef659b74ec8c464e468b91cf2bfd85ac6
gunesevitan <gunesevitan@gmail.com>
2023-02-02 19:55:31 +0300

    dataset

--- src/utilities/dataset_writer.py
+++ src/utilities/dataset_writer.py
@@ -17,12 +17,28 @@ if __name__ == '__main__':
     df_census_starter = pd.read_csv(settings.DATA / 'census_starter.csv')
     settings.logger.info(f'Test Set Shape: {df_census_starter.shape} Memory Usage: {df_census_starter.memory_usage().sum() / 1024 ** 2:.2f} MB')
 
+    for df in [df_train, df_test]:
+
+        df['cfips'] = df['cfips'].astype(np.uint32)
+        if 'active' in df.columns:
+            df['active'] = df['active'].astype(np.uint32)
+
+        df['first_day_of_month'] = pd.to_datetime(df['first_day_of_month'])
+        df['month'] = df['first_day_of_month'].dt.month.astype(np.uint8)
+        df['year'] = df['first_day_of_month'].dt.year.astype(np.uint16)
+
     for column in df_census_starter.columns:
         if df_census_starter[column].dtype == 'float64':
             df_census_starter[column] = df_census_starter[column].astype(np.float32)
 
         if df_census_starter[column].dtype == 'int64':
             df_census_starter[column] = df_census_starter[column].astype(np.uint32)
+
+    df_train.to_parquet(settings.DATA / 'train.parquet')
+    settings.logger.info(f'train.parquet is saved to {settings.DATA}')
+
+    df_test.to_parquet(settings.DATA / 'test.parquet')
+    settings.logger.info(f'test.parquet is saved to {settings.DATA}')
 
     df_census_starter.to_parquet(settings.DATA / 'census_starter.parquet')
     settings.logger.info(f'census_starter.parquet is saved to {settings.DATA}')
