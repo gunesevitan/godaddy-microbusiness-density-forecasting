ccd755a8b836a0804e2b88faa6dbd97d2f853566
gunesevitan <gunesevitan@gmail.com>
2023-02-21 09:59:47 +0300

    dataset writer

--- src/utilities/dataset_writer.py
+++ src/utilities/dataset_writer.py
@@ -10,12 +10,20 @@ if __name__ == '__main__':
 
     df_train = pd.read_csv(settings.DATA / 'train.csv')
     settings.logger.info(f'train Shape: {df_train.shape} Memory Usage: {df_train.memory_usage().sum() / 1024 ** 2:.2f} MB')
-    df_test = pd.read_csv(settings.DATA / 'test.csv')
 
+    df_test = pd.read_csv(settings.DATA / 'test.csv')
     settings.logger.info(f'test Shape: {df_test.shape} Memory Usage: {df_test.memory_usage().sum() / 1024 ** 2:.2f} MB')
+
+    df_revealed_test = pd.read_csv(settings.DATA / 'revealed_test.csv')
+    settings.logger.info(f'revealed_test Shape: {df_revealed_test.shape} Memory Usage: {df_revealed_test.memory_usage().sum() / 1024 ** 2:.2f} MB')
 
     df_census_starter = pd.read_csv(settings.DATA / 'census_starter.csv')
     settings.logger.info(f'census_starter Shape: {df_census_starter.shape} Memory Usage: {df_census_starter.memory_usage().sum() / 1024 ** 2:.2f} MB')
+
+    df_train = pd.concat((
+        df_train,
+        df_revealed_test
+    ), axis=0, ignore_index=True)
 
     for df in [df_train, df_test]:
 
@@ -26,6 +34,9 @@ if __name__ == '__main__':
         df['first_day_of_month'] = pd.to_datetime(df['first_day_of_month'])
         df['month'] = df['first_day_of_month'].dt.month.astype(np.uint8)
         df['year'] = df['first_day_of_month'].dt.year.astype(np.uint16)
+
+    df_train.sort_values(by=['cfips', 'first_day_of_month'], inplace=True)
+    df_train.reset_index(drop=True, inplace=True)
 
     for column in df_census_starter.columns:
         if df_census_starter[column].dtype == 'float64':
